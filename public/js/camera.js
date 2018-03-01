f222b1ac1ec13eb61dd088e3fb827696cc44479b
Maya Bello <mbello@ucsd.edu>
2018-03-01 10:15:48 -0800

    thirdParty API lab

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,8 +1,8 @@
 function initCamera() {
-  // Uncomment and fill in the correct selectors below.
-  // capture($('<Your code here>'),
-  //        $('<Your code here>'),
-  //        $('<Your code here>'));
+//   Uncomment and fill in the correct selectors below.
+//   capture($('#'),
+//          $('#'),
+//          $('#'));
     capture($('#camera-video'),
           $('#camera-canvas'),
           $('#camera-button'));
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -18,9 +18,40 @@ function initializePage() {
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	$(function() {
+	    $(".judge-img").bind("taphold", tapholdHandler);
+
+	    function tapholdHandler(event) {
+	        //get the id of the event space
+	        var targetIDPrefix = event.target.id;
+	        console.log("got prefix: " + targetIDPrefix);
+	        // show bio
+	        $("#" + targetIDPrefix + "-bio").show();
+
+	    }
+    });
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+  $('#rsvpForm').submit(function(e) {
+    e.preventDefault();
+    console.log("submitting form...");
+    var rsvpEmail = $('#rsvpEmail').val();
+    // send the POST request
+    $.post('addRSVP', { rsvpEmail: rsvpEmail}, postCallback);
+    });
+
+  function postCallback(res) {
+    alert("RSVP form successfully submitted!");
+    $('#rsvpEmail').val('');
+  }
 }
+
+$(function(){
+    $("div.box").bind("taphold", tapholdHandler);
+    function tapholdHandler( event ) {
+        $(event.target).addClass("taphold");
+    }
+});
